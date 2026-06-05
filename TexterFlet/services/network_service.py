685629d6e7cf052616554976a5e21dd1309f8a40
Anshumaan Soni <deadlygames.2006@gmail.com>
2026-06-06 01:12:25 +0530

    Fix: Update WebSocket and auth URLs for local network configuration

--- TexterFlet/services/network_service.py
+++ TexterFlet/services/network_service.py
@@ -22,8 +22,8 @@ class NetworkService:
         self.websocket = None
 
         if use_local:
-            self.ws_uri = "ws://127.0.0.1:8000/ws"
-            self.auth_url = "http://127.0.0.1:8000"
+            self.ws_uri = "ws://192.168.1.150:8000/ws"
+            self.auth_url = "http://192.168.1.150:8000"
         else:
             self.ws_uri = "wss://textere2ee-hvbahvb0gzfrf4bb.centralindia-01.azurewebsites.net/ws"
             self.auth_url = "https://textere2ee-hvbahvb0gzfrf4bb.centralindia-01.azurewebsites.net"
